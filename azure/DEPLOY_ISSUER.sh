7b9638273baf6da167c1045c5cd399954d326e22
Omer Shafiq <omer.shafiq2@gmail.com>
2020-04-29 01:46:05 +0300

    external script update

---
+++ azure/DEPLOY_ISSUER.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+az container create --resource-group trustnet-covid-initiative --name issuer-agent-container \
+    --image "${REGISTRY_LOGIN_SERVER}/aries-issuer-agent-image:latest" \
+    --registry-login-server "${REGISTRY_LOGIN_SERVER}" \
+    --registry-username $REGISTRY_USERNAME \
+    --registry-password $REGISTRY_PASSWORD \
+    --dns-name-label issuer \
+    --cpu 1 \
+    --memory 1 \
+    --ports [3000 8000]
