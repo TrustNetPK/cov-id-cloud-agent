7880fe4a4b10f441239a023a9cc95b2753b92213
Omer Shafiq <omer.shafiq2@gmail.com>
2020-04-29 02:01:51 +0300

    az command fix

--- azure/DEPLOY_ISSUER.sh
+++ azure/DEPLOY_ISSUER.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo ${REGISTRY_LOGIN_SERVER}
-echo ${REGISTRY_USERNAME}
-
 az container create --resource-group trustnet-covid-initiative --name issuer-agent-container \
     --image "${REGISTRY_LOGIN_SERVER}/aries-issuer-agent-image:latest" \
     --registry-login-server "${REGISTRY_LOGIN_SERVER}" \
@@ -12,4 +9,4 @@ az container create --resource-group trustnet-covid-initiative --name issuer-age
     --dns-name-label issuer \
     --cpu 1 \
     --memory 1 \
-    --ports [3000 8000]
+    --ports 3000 8000
